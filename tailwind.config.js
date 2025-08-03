e40c6c4411920b2149e2c394f1de97ee0ce29f54
prashant <mani007dec@gmail.com>
2025-08-03 11:38:13 +0530

    satta-3

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // scans all .html files in the root folder
+  theme: {
+    extend: {}, // for custom theme extension
+  },
+  plugins: [], // add plugins like forms/typography here
+}
